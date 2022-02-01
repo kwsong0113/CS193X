cc8eb0ae7d14b95e7a37221196abb704d7195f43
Kiwhan Song <kwsong0113@gmail.com>
2022-02-01 16:01:59 +0900

    Complete evil extension

--- HW2-Quiz-and-Extensions/evil-extension/page.js
+++ HW2-Quiz-and-Extensions/evil-extension/page.js
@@ -12,9 +12,30 @@ const MATCH_LIST = {
 
 function transformTextNodes(node) {
   // TODO(you): Implement this function! See HW spec for details.
+  if (node.nodeName === 'SCRIPT' || node.nodeName === 'STYLE') {
+    return;
+  }
+  if (node.nodeType === Node.TEXT_NODE) {
+    node.textContent = transformTextContent(node.textContent);
+  }
+  for (const child of node.childNodes) {
+    transformTextNodes(child);
+  }
+}
+
+function transformTextContent(content) {
+  const splitContent = content.split(/[ \n]/);
+  for (let i = 0; i < splitContent.length; i++) {
+    const word = splitContent[i];
+    if (word in MATCH_LIST) {
+      splitContent[i] = MATCH_LIST[word];
+    }
+  }
+  return splitContent.join(' ');
 }
 
 transformTextNodes(document.body);
 
 // Log statement to test that the extension loaded properly.
 console.log('Evil extension loaded!');
+console.log('Extension updated');
